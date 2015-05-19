c0bc0e69eda6c1259f73b4ad23cd2bfa4eebfaa5
Philipp Mildenberger <philipp.mildenberger@student.uibk.ac.at>
2015-05-19 17:35:32 +0200

    added a test for the highscoreList

--- Server/src/test/java/JsonConstructorTest.java
+++ Server/src/test/java/JsonConstructorTest.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 import communication_protocol.json.JsonConstructor;
 import data_structures.treasure.Quiz;
 import data_structures.treasure.Treasure;
+import data_structures.user.HighscoreList;
+import data_structures.user.Inventory;
 import data_structures.user.User;
 import org.junit.*;
 
@@ -13,6 +15,7 @@ import java.util.List;
 public class JsonConstructorTest {
 	List<Treasure> exampleTreasures;
 	List<User> exampleUsers;
+	List<HighscoreList> exampleHighscoreLists;
 	JsonConstructor jsonC;
 
 	@Before
@@ -26,36 +29,71 @@ public class JsonConstructorTest {
 		exampleTreasures.add(new Treasure(new Treasure.Location(10, 47.26952, 11.39570), null, new Treasure.Size(-1, 20, 1), null));
 		exampleTreasures.add(new Treasure(new Treasure.Location(10, 47.26952, 11.39570), quiz1, null, null));
 		exampleUsers = new ArrayList<>();
-		exampleUsers.add(new User("Hans","aasasdadsljaheoh", "hans@franz.at", 1234, 1, null));
+		List<Inventory.Entry> inventoryEntries = new ArrayList<>();
+		inventoryEntries.add(new Inventory.Entry(1, null));
+		inventoryEntries.add(new Inventory.Entry(2, null));
+		Inventory i = new Inventory(inventoryEntries);
+		exampleUsers.add(new User("Hans", "aasasdadsljaheoh", "hans@franz.at", 1234, 1, i));
+		exampleUsers.add(new User("Hans", "aasasdadsljaheoh", "hans@franz.at", 1234, 1, null));
+		exampleUsers.add(new User("Hans", null, "hans@franz.at", 1234, 1, i));
+		exampleHighscoreLists = new ArrayList<>();
+
+		List<HighscoreList.Entry> highscoreListEntries = new ArrayList<>();
+		highscoreListEntries.add(new HighscoreList.Entry("Hans", 1, 1234));
+		highscoreListEntries.add(new HighscoreList.Entry("Max", 2, 1134));
+		highscoreListEntries.add(new HighscoreList.Entry("Vanessa", 3, 666));
+		highscoreListEntries.add(new HighscoreList.Entry("Rupert", 4, 192));
+		highscoreListEntries.add(new HighscoreList.Entry("Mogli", 5, 128));
+		exampleHighscoreLists.add(new HighscoreList(1, 5, new ArrayList<HighscoreList.Entry>(highscoreListEntries)));
+		highscoreListEntries.add(new HighscoreList.Entry("Jaqueline", 6, 111));
+		exampleHighscoreLists.add(new HighscoreList(1, 6, new ArrayList<HighscoreList.Entry>(highscoreListEntries)));
+		highscoreListEntries.add(new HighscoreList.Entry("Chantal", 7, 99));
+		exampleHighscoreLists.add(new HighscoreList(1, 7, new ArrayList<HighscoreList.Entry>(highscoreListEntries)));
 		Collections.sort(exampleTreasures);
+		Collections.sort(exampleUsers);
+		Collections.sort(exampleHighscoreLists);
 	}
 
 	@Test
 	public void treasureTest() {
 		List<String> jsonExampleTreasures = new ArrayList<>();
-		for(Treasure t : exampleTreasures) {
+		for (Treasure t : exampleTreasures) {
 			jsonExampleTreasures.add(jsonC.toJson(t));
 		}
 		List<Treasure> result = new ArrayList<>();
-		for(String s : jsonExampleTreasures) {
-			result.add(jsonC.fromJson(s,Treasure.class));
+		for (String s : jsonExampleTreasures) {
+			result.add(jsonC.fromJson(s, Treasure.class));
 		}
 		Collections.sort(result);
 		assertEquals("the treasures aren't equal", exampleTreasures, result);
 	}
+
 	@Test
 	public void userTest() {
 		List<String> jsonExampleUsers = new ArrayList<>();
-		for(User u : exampleUsers) {
+		for (User u : exampleUsers) {
 			jsonExampleUsers.add(jsonC.toJson(u));
 		}
 		List<User> result = new ArrayList<>();
-		for(String s : jsonExampleUsers) {
-			result.add(jsonC.fromJson(s,User.class));
+		for (String s : jsonExampleUsers) {
+			result.add(jsonC.fromJson(s, User.class));
 		}
 		Collections.sort(result);
-		assertEquals("the treasures aren't equal", exampleUsers, result);
+		assertEquals("the users aren't equal", exampleUsers, result);
+	}
 
+	@Test
+	public void highscoreListTest() {
+		List<String> jsonExampleHighscoreLists = new ArrayList<>();
+		for (HighscoreList h : exampleHighscoreLists) {
+			jsonExampleHighscoreLists.add(jsonC.toJson(h));
+		}
+		List<HighscoreList> result = new ArrayList<>();
+		for (String s : jsonExampleHighscoreLists) {
+			result.add(jsonC.fromJson(s, HighscoreList.class));
+		}
+		Collections.sort(result);
+		assertEquals("the HighscoreLists aren't equal", exampleHighscoreLists, result);
 	}
 
 }
