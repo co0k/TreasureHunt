525bd19e2c0d819968e18b4554e478c86b31f2be
Philipp Mildenberger <philipp.mildenberger@student.uibk.ac.at>
2015-06-09 20:05:54 +0200

    the client/app now only sends hashed passwords(new users necessary), small bugfix also in registerUser

--- TreasureHuntApp/app/src/main/java/at/tba/treasurehunt/controller/AuthenticationController.java
+++ TreasureHuntApp/app/src/main/java/at/tba/treasurehunt/controller/AuthenticationController.java
@@ -6,6 +6,10 @@ package at.tba.treasurehunt.controller;
 
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import at.tba.treasurehunt.servercomm.ServerCommunication;
 import at.tba.treasurehunt.tasks.IResponseCallback;
 import at.tba.treasurehunt.utils.DummyDataProvider;
@@ -18,71 +22,104 @@ import communication_controller.json.JsonConstructor;
  */
 public class AuthenticationController implements IResponseCallback {
 
-    private static AuthenticationController instance = null;
+	private static AuthenticationController instance = null;
 
-    public static AuthenticationController getInstance(){
-        if (instance == null) instance = new AuthenticationController();
-        return instance;
-    }
+	public static AuthenticationController getInstance() {
+		if (instance == null) instance = new AuthenticationController();
+		return instance;
+	}
 
-    private AuthenticationController(){}
-    private IAuthenticationCallback callback;
-    private Integer userID = 0;
+	private AuthenticationController() {
+	}
 
-    public void authenticateUser(String uName, String password, IAuthenticationCallback callback){
-        this.callback = callback;
-        ServerCommunication.getInstance().logInToServer(uName, password, this);
-    }
+	private IAuthenticationCallback callback;
+	private Integer userID = 0;
 
-    public void registerNewUser(String uName, String email, String password, String passwordRetype, IAuthenticationCallback callback){
-        this.callback = callback;
-        if (!password.equals(passwordRetype)){
-            callback.onRegistrationError(AuthenticationError.REGISTRATION_WRONG_PASSWORD_RETYPE);
-        }
-        ServerCommunication.getInstance().registerUserOnServer(uName, email, password, this);
-    }
+	public void authenticateUser(String uName, String password, IAuthenticationCallback callback) {
+		this.callback = callback;
+		password = generateHash(uName + password);
+		ServerCommunication.getInstance().logInToServer(uName, password, this);
+	}
 
-    @Override
-    public void onResponseReceived(JSONRPC2Response response) {
-        JsonConstructor constr = new JsonConstructor();
+	public void registerNewUser(String uName, String email, String password, String passwordRetype, IAuthenticationCallback callback) {
+		this.callback = callback;
+		if (!password.equals(passwordRetype)) {
+			callback.onRegistrationError(AuthenticationError.REGISTRATION_WRONG_PASSWORD_RETYPE);
+			return;
+		}
+		password = generateHash(uName + password);
+		ServerCommunication.getInstance().registerUserOnServer(uName, email, password, this);
+	}
 
-        if (response.toJSONObject().get("id").equals("0")){
-            if (response.toJSONObject().get("result").equals("null")){
-                callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
-                return;
-            }
-            Integer result = constr.fromJson((String)(response.getResult())
-                    , Integer.class);
-            if (result == null) {
-                callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
-                return;
-            }
-            callback.onAuthenticationSuccess();
-            userID = result;
-        }else if (response.toJSONObject().get("id").equals("-1")){
-            if (response.toJSONObject().get("result").equals("null")){
-                callback.onRegistrationError(AuthenticationError.UNKNOWN_ERROR);
-                return;
-            }
-            Boolean result = constr.fromJson((String) response.getResult(), Boolean.class);
-            if (result == false) {
-                callback.onRegistrationError(AuthenticationError.UNKNOWN_ERROR);
-                return;
-            }
+	@Override
+	public void onResponseReceived(JSONRPC2Response response) {
+		JsonConstructor constr = new JsonConstructor();
 
-            callback.onRegistrationSuccess();
-        }else{
-            callback.onRegistrationError(AuthenticationError.UNKNOWN_ERROR);
-            callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
-        }
-    }
+		if (response.toJSONObject().get("id").equals("0")) {
+			if (response.toJSONObject().get("result").equals("null")) {
+				callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
+				return;
+			}
+			Integer result = constr.fromJson((String) (response.getResult())
+					, Integer.class);
+			if (result == null) {
+				callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
+				return;
+			}
+			callback.onAuthenticationSuccess();
+			userID = result;
+		} else if (response.toJSONObject().get("id").equals("-1")) {
+			if (response.toJSONObject().get("result").equals("null")) {
+				callback.onRegistrationError(AuthenticationError.UNKNOWN_ERROR);
+				return;
+			}
+			Boolean result = constr.fromJson((String) response.getResult(), Boolean.class);
+			if (result == false) {
+				callback.onRegistrationError(AuthenticationError.UNKNOWN_ERROR);
+				return;
+			}
 
-    @Override
-    public void onResponseReceiveError() {
-        callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
-    }
+			callback.onRegistrationSuccess();
+		} else {
+			callback.onRegistrationError(AuthenticationError.UNKNOWN_ERROR);
+			callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
+		}
+	}
 
-    public Integer getLoggedInUserID(){
-        return userID;
-    }
+	public static String generateHash(String toHash) {
+		MessageDigest md = null;
+		byte[] hash = null;
+		try {
+			md = MessageDigest.getInstance("SHA-512");
+			hash = md.digest(toHash.getBytes("UTF-8"));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return convertToHex(hash);
+	}
+
+	/**
+	 * Converts the given byte[] to a hex string.
+	 *
+	 * @param raw the byte[] to convert
+	 * @return the string the given byte[] represents
+	 */
+	private static String convertToHex(byte[] raw) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < raw.length; i++) {
+			sb.append(Integer.toString((raw[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
+	}
+
+	@Override
+	public void onResponseReceiveError() {
+		callback.onAuthenticationFailure(AuthenticationError.UNKNOWN_ERROR);
+	}
+
+	public Integer getLoggedInUserID() {
+		return userID;
+	}
 }
